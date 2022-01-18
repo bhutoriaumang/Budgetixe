0abda6eb1f2885eb598cb825abef90367aeb241f
Umang Bhutoria <bhutoriaumang@gmail.com>
2022-01-18 17:26:40 +0530

    Fixed responsiveness of the website

--- src/App.js
+++ src/App.js
@@ -18,54 +18,62 @@ function App() {
     <Router>
       <div className="App">
         <div className="nav-bar">
-          <div className="home-tag">
-            <IconContext.Provider value={{ className: "home-icon" }}>
-              <FaHome />
-            </IconContext.Provider>
-            <Link to="/dashboard">Dashboard</Link>
-          </div>
-          <br />
-          <div className="input-tag">
-            <IconContext.Provider value={{ className: "input-icon" }}>
-              <FaWpforms />
-            </IconContext.Provider>
-            <Link to="/input">Input Page</Link>
-          </div>
-          <br />
-          <div className="stock-tag">
-            <IconContext.Provider value={{ className: "stock-icon" }}>
-              <BsGraphUp />
-            </IconContext.Provider>
-            <Link to="/stock">Stock Page</Link>
-          </div>
-          <br />
-          <div className="crypto-tag">
-            <IconContext.Provider value={{ className: "crypto-icon" }}>
-              <BsCurrencyBitcoin />
-            </IconContext.Provider>
-            <Link to="/crypto">Crypto Page</Link>
-          </div>
-          <br />
-          <div className="subscription-tag">
-            <IconContext.Provider value={{ className: "subscription-icon" }}>
-              <BiMoney />
-            </IconContext.Provider>
-            <Link to="/subscription">Subscription Page</Link>
-          </div>
-          <br />
-          <div className="budget-tag">
-            <IconContext.Provider value={{ className: "budget-icon" }}>
-              <BiDollar />
-            </IconContext.Provider>
-            <Link to="/budget">Budget Page</Link>
-          </div>
-          <br />
-          <div className="goals-tag">
-            <IconContext.Provider value={{ className: "goals-icon" }}>
-              <GiStairsGoal />
-            </IconContext.Provider>
-            <Link to="/goals">Goals Page</Link>
-          </div>
+          <Link to="/dashboard">
+            <div className="dashboard-tag">
+              <IconContext.Provider value={{ className: "dashboard-icon" }}>
+                <FaHome />
+              </IconContext.Provider>
+              <p>Dashboard</p>
+            </div>
+          </Link>
+          <Link to="/input">
+            <div className="input-tag">
+              <IconContext.Provider value={{ className: "input-icon" }}>
+                <FaWpforms />
+              </IconContext.Provider>
+              <p>Input</p>
+            </div>
+          </Link>
+          <Link to="/stock">
+            <div className="stock-tag">
+              <IconContext.Provider value={{ className: "stock-icon" }}>
+                <BsGraphUp />
+              </IconContext.Provider>
+              <p>Stock</p>
+            </div>
+          </Link>
+          <Link to="/crypto">
+            <div className="crypto-tag">
+              <IconContext.Provider value={{ className: "crypto-icon" }}>
+                <BsCurrencyBitcoin />
+              </IconContext.Provider>
+              <p>Crypto</p>
+            </div>
+          </Link>
+          <Link to="/budget">
+            <div className="budget-tag">
+              <IconContext.Provider value={{ className: "budget-icon" }}>
+                <BiDollar />
+              </IconContext.Provider>
+              <p>Budget</p>
+            </div>
+          </Link>
+          <Link to="/subscription">
+            <div className="subscription-tag">
+              <IconContext.Provider value={{ className: "subscription-icon" }}>
+                <BiMoney />
+              </IconContext.Provider>
+              <p>Subscription</p>
+            </div>
+          </Link>
+          <Link to="/goals">
+            <div className="goals-tag">
+              <IconContext.Provider value={{ className: "goals-icon" }}>
+                <GiStairsGoal />
+              </IconContext.Provider>
+              <p>Goals</p>
+            </div>
+          </Link>
         </div>
         <div className="main-page">
           <Switch>
