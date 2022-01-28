1b669dccd70b38b0388a1a4ad413dc7100323d0a
Umang Bhutoria <bhutoriaumang@gmail.com>
2022-01-28 18:45:54 +0530

    Worked on the stocks page

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -8,36 +8,32 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const Chart = ({ pdata,priceChange }) => {
+const Chart = ({ pdata, priceChange }) => {
   return (
     <>
       <ResponsiveContainer width="100%" height="100%">
-                <AreaChart
-                  data={pdata}
-                  margin={{
-                    top: 0,
-                    right: 0,
-                    left: 0,
-                    bottom: 0,
-                  }}
-                >
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis
-                    dataKey="name"
-                    fontSize="0"
-                    interval={"preserveStartEnd"}
-                  />
-                  <YAxis />
-                  <Tooltip />
-                  <Area
-                    type="monotone"
-                    dataKey="value"
-                    stroke={priceChange > 0 ? "green" : "red"}
-                    fill={priceChange > 0 ? "lightgreen" : "lightcoral"}
-                    fillOpacity="0.3"
-                  />
-                </AreaChart>
-              </ResponsiveContainer>
+        <AreaChart
+          data={pdata}
+          margin={{
+            top: 0,
+            right: 0,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" fontSize="0" interval={"preserveStartEnd"} />
+          <YAxis />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="value"
+            stroke={priceChange > 0 ? "green" : "red"}
+            fill={priceChange > 0 ? "lightgreen" : "lightcoral"}
+            fillOpacity="0.3"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
     </>
   );
 };
